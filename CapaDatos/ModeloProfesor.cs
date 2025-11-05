126708dd9aaf59021de97df9b9f19ef33d5cb6ad
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-05 06:25:27 -0500

    [ModeloProfesorOk]

---
+++ CapaDatos/ModeloProfesor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class ModeloProfesor
+    {
+        public int idIdentity { get; set; }
+        public int id { get; set; }
+        public string nombre { get; set; }
+        
+        public ModeloProfesor()
+        {
+            idIdentity = 0;
+            id = 0;
+            nombre = string.Empty;
+        }
+    }
+}
