2225085bf512553cd8f39a02c23eb917503ede8b
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-05 17:57:51 -0500

    [MODIFIED].[CapaDatos].[TestCreateProfesor].[OK]

---
+++ CapaTesting/Testing CapaDatos/Testing CreateProfesor.cs
@@ -0,0 +1,38 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaTesting.Testing_CapaDatos
+{
+    [TestClass]
+    public class Testing_CreateProfesor
+    {
+        [TestMethod]
+        public void TestCreateProfesor()
+        {
+            try
+            {
+                ModeloProfesor nuevoRegistro = new ModeloProfesor
+                {
+                    id = 9999,
+                    nombre = "Profesor Test"
+                };
+
+                CrudAngularNetAppi objCreate = new CrudAngularNetAppi();
+                bool resultado = objCreate.CreateProfesor(nuevoRegistro);
+
+                Assert.IsTrue(resultado, "[Test Create Profesor] La creación del registro fue exitosa.");
+                Console.WriteLine("[****].[OK].[Test Create Profesor] La creación del registro fue exitosa.");
+
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"[Test Create Profesor] La prueba falló con una excepción: + {ex.Message}");
+                Console.WriteLine($"[ERROR].[Test Create Profesor] La prueba falló con una excepción: +  {ex.Message}");
+            }
+        }
+    }
+}
