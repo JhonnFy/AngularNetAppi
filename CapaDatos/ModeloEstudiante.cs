8842b049f765daf89378c9015b427c15679d46b7
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-05 06:19:50 -0500

    [ModeloEstudianteOk]

---
+++ CapaDatos/ModeloEstudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CapaDatos
+{
+    public class ModeloEstudiante
+    {
+        public int idIdentity { get; set; }
+        public long id { get; set; }
+        public string nombre { get; set; }
+
+        public ModeloEstudiante()
+        {
+            idIdentity = 0;
+            id = 0;
+            nombre = string.Empty;
+        }
+    }
+}
