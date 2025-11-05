275c0e369aa51af5265de1a91f42156274e89719
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-05 06:31:20 -0500

    [MODIFIED].[CapaDatos].[ModeloEstudianteOk]

---
+++ CapaDatos/ModeloNota.cs
@@ -0,0 +1,19 @@
+﻿using Azure.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class ModeloNota
+    {
+        public int idIdentity { get; set; }
+        public int id {  get; set; }
+        public string nombre { get; set; }
+        public int idProfesor { get; set; }
+        public int idEstudiante { get; set; }
+        public decimal valor { get; set; }
+    }
+}
