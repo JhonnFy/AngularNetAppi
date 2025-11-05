65438fe17acd776d2d6a583d7fc10ee08a568976
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-05 10:33:09 -0500

    [CREATE].[CapaDatos].[ReadProfesor]

--- CapaDatos/CrudAngularNetAppi.cs
+++ CapaDatos/CrudAngularNetAppi.cs
@@ -50,6 +50,42 @@ namespace CapaDatos
             return listaReadEstudiante;
         }
 
+        public List<ModeloProfesor> ReadProfesor()
+        {
+            var listaReadProfesor = new List<ModeloProfesor>();
+            try
+            {
+                using (var db = conexion.ObtenerConexion())
+                {
+                    db.Open();
+                    string @read =
+                        "SELECT * FROM Profesor";
+
+                    using (SqlCommand readSql = new SqlCommand(read, db))
+                    using (SqlDataReader runReadSql = readSql.ExecuteReader())
+                    {
+                        while (runReadSql.Read())
+                        {
+                            var modelo = new ModeloProfesor
+                            {
+                                idIdentity = runReadSql.GetInt32(0),
+                                id = runReadSql.GetInt32(1),
+                                nombre = runReadSql.GetString(2),
+                            };
+                            listaReadProfesor.Add(modelo);
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("[****].[ERROR].[Capa CrudCodigoDeBarrasOrigen].[ReadProfesor]");
+                throw new Exception("ERROR [Capa CrudCodigoDeBarrasOrigen].[ReadProfesor] " + ex.Message);
+            }
+            return listaReadProfesor;
+        }
+
 
     }
 }
