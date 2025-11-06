7fda6b0704cf2794a618173edf98fca5db8de1a0
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-05 23:49:15 -0500

    [CapaNegocio_Nota].[OK]

--- CapaNegocio/CapaNegocio Nota.cs
+++ CapaNegocio/CapaNegocio Nota.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaDatos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,55 @@ using System.Threading.Tasks;
 
 namespace CapaNegocio
 {
-    internal class CapaNegocio_Nota
+    public class CapaNegocio_Nota
     {
+        private CrudAngularNetAppi crudDatos = new CrudAngularNetAppi();
+
+        public List<ModeloNota> NegocioReadNota()
+        {
+            var lista = crudDatos.ReadNota();
+            foreach (var nota in lista)
+            {
+                nota.nombre = ReglasNegocioHelper.HelperNombreNota(nota.nombre);
+                nota.valor = ReglasNegocioHelper.HelperAjustarValoresNotas(nota.valor);
+            }
+            return lista;
+        }
+
+        public List<ModeloNota> NegocioReadNotaId(int id)
+        {
+            var lista = crudDatos.ReadNotaId(id);
+
+            foreach (var notaId in lista)
+            {
+                notaId.nombre = ReglasNegocioHelper.HelperNombreNota(notaId.nombre);
+                notaId.valor = ReglasNegocioHelper.HelperAjustarValoresNotas(notaId.valor);
+            }
+
+            return lista;
+        }
+
+        public bool NegocioCreateNota(ModeloNota nuevaNota)
+        {
+            if (nuevaNota == null)
+                throw new ArgumentNullException(nameof(nuevaNota));
+
+            nuevaNota.nombre = ReglasNegocioHelper.HelperNombreNota(nuevaNota.nombre);
+            nuevaNota.valor = ReglasNegocioHelper.HelperAjustarValoresNotas(nuevaNota.valor);
+
+            return crudDatos.CreateNota(nuevaNota);
+        }
+
+        public bool NegocioUpdateNota(ModeloNota actualizarNota)
+        {
+            if (actualizarNota == null)
+                throw new ArgumentException(nameof(actualizarNota));
+
+            actualizarNota.nombre = ReglasNegocioHelper.HelperNombreNota(actualizarNota.nombre);
+            actualizarNota.valor = ReglasNegocioHelper.HelperAjustarValoresNotas(actualizarNota.valor);
+
+            return crudDatos.UpdateNotas(actualizarNota);
+        }
+
     }
 }
--- CapaNegocio/CapaNegocio Profesor.cs
+++ CapaNegocio/CapaNegocio Profesor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaDatos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace CapaNegocio
 {
-    internal class CapaNegocio_Profesor
+    public class CapaNegocio_Profesor
     {
+        private CrudAngularNetAppi crudDatos = new CrudAngularNetAppi();
     }
 }
