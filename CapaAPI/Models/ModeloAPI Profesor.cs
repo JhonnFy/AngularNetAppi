7a17287eac8276e1b2206d7d1e434b419438721e
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-06 10:00:59 -0500

    [BugIdentity].[Reescribiendo].[ModeloAPI Profesor]

--- CapaAPI/Models/ModeloAPI Profesor.cs
+++ CapaAPI/Models/ModeloAPI Profesor.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace CapaAPI.Models
+{
+    public class Profesor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public Profesor()
+        {
+            Id = 0;
+            Nombre = string.Empty;
+        }
+    }
+}
