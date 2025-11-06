689c4b33f193c9cadfbcbfc7868d15d4b0d629e7
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-06 01:02:43 -0500

    [ConstructorReadProfesor]

--- CapaControlador/CapaControlador Profesor.cs
+++ CapaControlador/CapaControlador Profesor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaNegocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace CapaControlador
 {
-    internal class CapaControlador_Profesor
+    public class CapaControladorProfesor
     {
+        private CapaNegocio_Profesor objNegocioProfesor;
+        
+        public CapaControladorProfesor()
+        {
+            objNegocioProfesor = new CapaNegocio_Profesor();
+        }
+            
+
+
+
+
     }
 }
