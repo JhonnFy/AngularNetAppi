61718d62c9668e167553efd42c3fd2d6cfd3e0cc
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-05 19:31:13 -0500

    [MODIFIED].[CapaDatos].[UpdateProfesor].[OK]

---
+++ CapaTesting/Testing CapaDatos/Test UpdateProfesor.cs
@@ -0,0 +1,37 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaTesting.Testing_CapaDatos
+{
+    [TestClass]
+    public class Test_UpdateProfesor
+    {
+        [TestMethod]
+        public void TestUpdateProfesor()
+        {
+            try
+            {
+                ModeloProfesor objUpProfesor = new ModeloProfesor
+                {
+                    id = 1111,
+                    nombre = "Test Andres Daniel Sanchez Rojas:)"
+                };
+
+                CrudAngularNetAppi sqlUpdate = new CrudAngularNetAppi();
+                bool resultado = sqlUpdate.UpdateProfesor(objUpProfesor);
+
+                Assert.IsTrue(resultado, "[Test Update Profesor] La actualización del registro fue exitosa.");
+                Console.WriteLine("[Test Update Profesor] La actualización del registro fue exitosa");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"[Test Update Profesor] La prueba falló con una excepción: {ex.Message}");
+            }
+        }
+
+    }
+}
