8782b6b3dc7ab1d8bb8d71e8e7be9e1437f08294
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-05 20:25:02 -0500

    [MODIFIED].[CapaDatos].[DeleteEstudiante].[OK]

---
+++ CapaTesting/Testing CapaDatos/Test DeleteEstudiante.cs
@@ -0,0 +1,36 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaTesting.Testing_CapaDatos
+{
+    [TestClass]
+    public class Test_DeleteEstudiante
+    {
+        [TestMethod]
+        public void TestDeleteEstudiante()
+        {
+            try
+            {
+                ModeloEstudiante borrarRegistro = new ModeloEstudiante
+                {
+                    id = 1307
+                };
+
+                CrudAngularNetAppi objDelete = new CrudAngularNetAppi();
+                bool resultado = objDelete.DeleteEstudiante(borrarRegistro);
+
+                Assert.IsTrue(resultado, "[Delete Estudiante] El registro fue eliminado correctamente.");
+                Console.WriteLine("[Delete Estudiante] El registro fue eliminado correctamente.");
+
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"[Test Delete Estudiante] La prueba falló con una excepción: {ex.Message}");
+            }
+        }
+    }
+}
