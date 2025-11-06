2f7b2d875dfeffc44dea48eaca91b5e33a28cb2c
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-05 19:17:45 -0500

    [MODIFIED].[CapaDatos].[UpdateEstudiante]

---
+++ CapaTesting/Testing CapaDatos/Test UpdateEstudiante.cs
@@ -0,0 +1,37 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaTesting
+{
+    [TestClass]
+    public class Test_UpdateEstudiante
+    {
+        [TestMethod]
+        public void TestUpdateEstudiante()
+        {
+            try
+            {
+                ModeloEstudiante objUpdateEstudiante = new ModeloEstudiante
+                {
+                    id = 1307,
+                    nombre = "Test Natalia Mariana Morales Sanchez V1"
+                };
+
+                CrudAngularNetAppi sqlUpdate = new CrudAngularNetAppi();
+                bool resultado = sqlUpdate.UpdateEstudiante(objUpdateEstudiante);
+
+                Assert.IsTrue(resultado, "[Test Update Estudiante] La actualización del registro fue exitosa.");
+                Console.WriteLine("[Test Update Estudiante] La actualización del registro fue exitosa");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"[Test Update Estudiante] La prueba falló con una excepción: {ex.Message}");
+            }
+        }
+
+    }
+}
