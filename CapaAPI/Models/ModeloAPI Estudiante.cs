4f1452e7b274a4835cadc63d8d3238db9773493c
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-06 09:58:13 -0500

    [BugIdentity].[Reescribiendo].[ModeloAPI Estudiante]

--- CapaAPI/Models/ModeloAPI Estudiante.cs
+++ CapaAPI/Models/ModeloAPI Estudiante.cs
@@ -1,1 +1,14 @@
-﻿
+﻿namespace CapaAPI.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public Estudiante()
+        {
+            Id = 0;
+            Nombre = string.Empty;
+        }
+    }
+}
