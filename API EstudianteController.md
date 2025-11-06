2c3365194c621b6f999a2272de1eb2196d20d4a2
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-06 03:18:34 -0500

    [Controller]

---
+++ API EstudianteController.md
@@ -0,0 +1,5 @@
+NegocioReadEstudiante() // GET todos
+NegocioReadEstudianteId(id) // GET por id
+NegocioReadCreateEstudiante(nuevo) // POST
+NegocioUpdateEstudiante(actualizar) // PUT
+NegocioEliminarEstudiante(id) // DELETE
---
+++ CapaAPI/Controllers/EstudianteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CapaDatos;
+using CapaNegocio;
+
+namespace CapaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstudianteController : ControllerBase
+    {
+        private readonly CapaNegocio_Estudiante _negocioEstudiante;
+
+        public EstudianteController()
+        {
+            _negocioEstudiante = new CapaNegocio_Estudiante();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var lista = _negocioEstudiante.NegocioReadEstudiante();
+            return Ok(lista);
+        }
+
+
+    }
+}
