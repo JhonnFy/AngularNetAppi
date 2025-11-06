b87b0240c8d52b262f9219aae04aecb35388551a
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-05 19:43:49 -0500

    [MODIFIED].[CapaDatos].[TestUpdateNotas].[OK]

---
+++ CapaTesting/Testing CapaDatos/Test UpdateNota.cs
@@ -0,0 +1,38 @@
+﻿using CapaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaTesting.Testing_CapaDatos
+{
+    [TestClass]
+    public class Test_UpdateNota
+    {
+        [TestMethod]
+        public void TestUpdateNotas()
+        {
+            try
+            {
+                ModeloNota objUpdateNota = new ModeloNota
+                {
+                    id = 1,
+                    nombre = "Corte Recuperacion TEST",
+                    valor = 4
+                };
+
+                CrudAngularNetAppi sqlUpdate = new CrudAngularNetAppi();
+                bool resultado = sqlUpdate.UpdateNotas(objUpdateNota);
+
+                Assert.IsTrue(resultado, "[Test Update Notas] La actualización del registro fue exitosa.");
+                Console.WriteLine("[Test Update Notas] La actualización del registro fue exitosa");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"[Test Update Notas] La prueba falló con una excepción: {ex.Message}");
+            }
+        }
+
+    }
+}
