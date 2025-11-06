deebae63d20bb4d8d4565018a6d3a128f5b08492
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-06 10:00:00 -0500

    [BugIdentity].[Reescribiendo].[ModeloAPI Nota]

--- CapaAPI/Models/ModeloAPI Nota.cs
+++ CapaAPI/Models/ModeloAPI Nota.cs
@@ -1,1 +1,20 @@
-﻿
+﻿namespace CapaAPI.Models
+{
+    public class Nota
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int IdProfesor { get; set; }
+        public int IdEstudiante { get; set; }
+        public decimal Valor { get; set; }
+
+        public Nota()
+        {
+            Id = 0;
+            Nombre = string.Empty;
+            IdProfesor = 0;
+            IdEstudiante = 0;
+            Valor = 0;
+        }
+    }
+}
