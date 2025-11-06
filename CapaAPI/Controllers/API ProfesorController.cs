fb26aeea355d5419b538f529b430fae85bd0c05c
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-06 02:14:54 -0500

    [CREATE].[Controladores API]

---
+++ CapaAPI/Controllers/API ProfesorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class API_ProfesorController : ControllerBase
+    {
+    }
+}
