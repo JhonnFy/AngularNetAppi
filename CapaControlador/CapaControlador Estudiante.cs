5cf2cc64c0816ac8975b31847a95a75718b1775a
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-06 00:35:59 -0500

    [ControladorReadEstudiante]

--- CapaControlador/CapaControlador Estudiante.cs
+++ CapaControlador/CapaControlador Estudiante.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CapaNegocio;
+using CapaDatos;
 
 namespace CapaControlador
 {
-    internal class CapaControlador_Estudiante
+    public class ControladorEstudiante
     {
+        private CapaNegocio_Estudiante objNegocioEstudiante;
+
+        public ControladorEstudiante()
+        {
+            objNegocioEstudiante = new CapaNegocio_Estudiante();
+        }
+
+
     }
 }
