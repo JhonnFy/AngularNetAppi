682cff929ea4b63c6e27f802dec088a04bf64eac
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-08 05:16:36 -0500

    [CREATE].[FRONT Profesores]

--- FrontAngularNetAppi/src/app/app.routes.ts
+++ FrontAngularNetAppi/src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { EstudianteComponent } from './estudiante/estudiante';
 import { Dashboard } from './dashboard/dashboard'; // tu dashboard
+import { ProfesorComponent } from './profesor/profesor';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: Dashboard },
-  { path: 'estudiantes', component: EstudianteComponent } // ✅ solo estudiantes por ahora
+  { path: 'estudiantes', component: EstudianteComponent },
+  { path: 'profesores', component: ProfesorComponent }
+
 ];
 
 
