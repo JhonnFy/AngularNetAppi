256531e6a8c224efea4a90f6ca24a2814c8e8ffa
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-08 22:52:48 -0500

    [Front Notas V2].[notas.ts]

--- FrontAngularNetAppi/src/app/nota/nota.ts
+++ FrontAngularNetAppi/src/app/nota/nota.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { NotaService, Nota } from '../nota/services/services';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterModule } from '@angular/router'; // <- RouterModule
 import { Observable } from 'rxjs';
 import Swal from 'sweetalert2';
 
@@ -11,54 +11,59 @@ import Swal from 'sweetalert2';
   selector: 'app-nota',
   templateUrl: './nota.html',
   styleUrls: ['./nota.scss'], 
-  standalone: true
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ]
 })
-export class NotaComponent {
+export class NotaComponent implements OnInit { 
     notas: Nota[] = [];
-      paginaActual = 1;
-      registrosPorPagina = 7;
-      filaEditandoId: number | null = null;
-      nombreEditado: string = ''; 
-  
+    paginaActual = 1;
+    registrosPorPagina = 7;
+    filaEditandoId: number | null = null;
+    nombreEditado: string = ''; 
+    filtro: string = ''; 
+
     constructor(
       private notaservice: NotaService,
       private http: HttpClient,
       private router: Router
     ) {}
-  
+
     ngOnInit() {
-
-    }
-
-  cargarNotas() {
-    this.notaservice.getNotas().subscribe((data: Nota[]) => {
-      this.notas = data;
-    }, (err) => console.error('Error cargando notas', err));
-  }
- 
-  filtro: string = ''; 
-  aplicarFiltro() {
-    if(!this.filtro){
       this.cargarNotas();
-      return;
     }
-  
-    const id = +this.filtro;
-    this.notaservice.getNotaPorId(+this.filtro).subscribe({
-    next: (nota: Nota) => {
-      this.notas = this.notas ? [nota] : [];
+
+    cargarNotas() {
+      this.notaservice.getNotas().subscribe((data: Nota[]) => {
+        this.notas = data;
+      }, (err) => console.error('Error cargando notas', err));
+    }
+
+    aplicarFiltro() {
+      if(!this.filtro){
+        this.cargarNotas();
+        return;
+      }
+
+      const id = +this.filtro;
+      this.notaservice.getNotaPorId(id).subscribe({
+        next: (nota: Nota) => {
+          this.notas = nota ? [nota] : [];
+          this.paginaActual = 1;
+        },
+        error: (err) => {
+          console.error('Error filtrando nota', err);
+          this.notas = [];
+        }
+      }); 
+    }
+
+    limpiarFiltro() {
+      this.filtro = '';
       this.paginaActual = 1;
-    },
-    error: (err) => {
-      console.error('Error filtrando nota', err);
-      this.notas = [];
+      this.cargarNotas();
     }
-    }); 
-  }
-
-
-
-
-
-
 }
