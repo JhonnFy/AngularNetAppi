07aed2619f96763ca6f409337e64f03dd1a94837
Jhonn Fy <moesDeveloper1023@gmail.com>
2025-11-10 02:01:16 -0500

    [Front]

--- CapaAPI/Controllers/ControllerAPI Nota.cs
+++ CapaAPI/Controllers/ControllerAPI Nota.cs
@@ -66,16 +66,16 @@ namespace CapaAPI.Controllers
             return NoContent();
         }
 
-        [HttpPost]
-        public IActionResult RegistrarNota([FromBody] Nota nota)
-        {
-            if (nota == null) return BadRequest("Datos inválidos");
+        //[HttpPost]
+        //public IActionResult RegistrarNota([FromBody] Nota nota)
+        //{
+        //    if (nota == null) return BadRequest("Datos inválidos");
 
-            var registrada = _servicio.RegistrarNota(nota);
-            if (!registrada)
-                return BadRequest("El profesor o el estudiante no existen.");
+        //    var registrada = _servicio.RegistrarNota(nota);
+        //    if (!registrada)
+        //        return BadRequest("El profesor o el estudiante no existen.");
 
-            return Ok(nota);
-        }
+        //    return Ok(nota);
+        //}
     }
 }
